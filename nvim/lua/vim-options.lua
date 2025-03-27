014433ef1008ffe5bcb214ecb2aef644245b21a9
alexitemoi <alexitemoi@disroot.org>
2025-03-27 15:57:30 +0000

    handle notebooks

--- nvim/lua/vim-options.lua
+++ nvim/lua/vim-options.lua
@@ -2,8 +2,8 @@ vim.cmd("set expandtab")
 vim.cmd("set tabstop=2")
 vim.cmd("set softtabstop=2")
 vim.cmd("set shiftwidth=2")
-vim.g.mapleader= " "
+vim.g.mapleader = " "
 vim.opt.number = true
 vim.opt.relativenumber = true
 vim.opt.colorcolumn = "80,100,120"
-
+vim.g.python3_host_prog = vim.fn.expand("~/pyvim/bin/python3")
